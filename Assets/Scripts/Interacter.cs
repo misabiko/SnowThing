e25b3b445989d3327eea33eb6e7a8f62d07a82cc
misabiko <misabiko@gmail.com>
2019-11-26 21:12:38 -0500

    Added interacter + Sorted hierarchy a bit

---
+++ Assets/Scripts/Interacter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Interacter : MonoBehaviour {
+	List<SnowBall> snowBalls = new List<SnowBall>();
+	
+	void OnTriggerEnter(Collider other) {
+		SnowBall snowBall = other.GetComponent<SnowBall>();
+		if (!snowBall) return;
+		
+		snowBall.GetComponent<Renderer>().material.SetColor("_BaseColor", Color.cyan);
+
+		snowBalls.Add(snowBall);
+	}
+
+	void OnTriggerExit(Collider other) {
+		SnowBall snowBall = other.GetComponent<SnowBall>();
+		if (!snowBall) return;
+		
+		snowBall.GetComponent<Renderer>().material.SetColor("_BaseColor", Color.white);
+
+		snowBalls.Remove(snowBall);
+	}
+
+	//Get the ball closer to be in front of the player
+	public SnowBall GetSnowBall() {
+		if (snowBalls.Count == 0)
+			return null;
+		
+		SnowBall snowBall = snowBalls[0];
+		float angle = GetAngle(snowBall);
+
+		foreach (SnowBall ball in snowBalls) {
+			if (GetAngle(ball) < angle)
+				snowBall = ball;
+		}
+
+		return snowBall;
+	}
+
+	float GetAngle(SnowBall snowBall) {
+		return Vector2.Angle(
+			Helper.RemoveY(transform.forward),
+			Helper.RemoveY(snowBall.transform.position - transform.position)
+		);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,15 +11,17 @@ public class PlayerController : MonoBehaviour {
 	Vector2 moveInput;
 	Material material;
 	Color defaultColor;
+	SnowBall snowBall;
 
 	public CinemachineFreeLook cam;
 	public Transform camTransform;
 	public float camYSensitivity = 0.01f;
 	public float camXSensitivity = 2f;
+
+	public Interacter interacter;
 	
 	public float speed = 5f;
 
-	public SnowBall snowBall;
 	public float ballPush = 50f;
 	public float pushRadius = 3f;
 	public float pushAngle = 45f;
@@ -32,14 +34,17 @@ public class PlayerController : MonoBehaviour {
 		material = GetComponent<Renderer>().material;
 		defaultColor = material.GetColor(BaseColor);
 		
-		if (cam == null)
+		if (!cam)
 			Debug.LogError("FreeLook camera wasn't set on PlayerController.");
-		if (camTransform == null)
+		if (!camTransform)
 			Debug.LogError("Camera transform (Main Camera) wasn't set on PlayerController.");
+		if (!interacter)
+			Debug.LogError("Interacter wasn't set on PlayerController.");
 
 		playerInput.actions["Move"].started += OnMove;
 		playerInput.actions["Move"].performed += OnMove;
 		playerInput.actions["Move"].canceled += OnMove;
+		
 		playerInput.actions["Push"].started += OnPushStart;
 		playerInput.actions["Push"].canceled += OnPushStop;
 
@@ -52,10 +57,14 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void OnPushStart(InputAction.CallbackContext context) {
+		snowBall = interacter.GetSnowBall();
+		
 		material.SetColor(BaseColor, Color.red);
 	}
 
 	void OnPushStop(InputAction.CallbackContext context) {
+		snowBall = null;
+		
 		material.SetColor(BaseColor, defaultColor);
 	}
 
@@ -70,19 +79,23 @@ public class PlayerController : MonoBehaviour {
 			Vector3 moveDirection = camTransform.forward * moveInput.y + camTransform.right * moveInput.x;
 			characterController.SimpleMove(speed * moveDirection);
 
-			if (snowBall) {
-				Vector2 pushDirection2d = Helper.RemoveY(snowBall.transform.position) - Helper.RemoveY(transform.position);
-				Vector3 pushDirection3d = new Vector3(pushDirection2d.x, 0f, pushDirection2d.y);
-				Vector2 moveDirection2d = Helper.RemoveY(moveDirection);
-
-				if (Vector2.Angle(moveDirection2d, pushDirection2d) < pushAngle) {
-					//TODO make that readable
-					float modifier = 1f - Mathf.Clamp01(Vector2.Dot(pushDirection2d, moveDirection2d) / (pushRadius + snowBall.radius));
-					snowBall.Push(modifier * ballPush * pushDirection3d.normalized);
-				}
-			}
-		}
-		else
+			PushSnowBall(moveDirection);
+		}else
 			characterController.SimpleMove(Vector3.zero);
+	}
+
+	void PushSnowBall(Vector3 moveDirection) {
+		if (!snowBall) return;
+		
+		Vector2 pushDirection2d = Helper.RemoveY(snowBall.transform.position) - Helper.RemoveY(transform.position);
+		Vector3 pushDirection3d = new Vector3(pushDirection2d.x, 0f, pushDirection2d.y);
+		Vector2 moveDirection2d = Helper.RemoveY(moveDirection);
+
+		//if the ball is in the push fov
+		if (!(Vector2.Angle(moveDirection2d, pushDirection2d) < pushAngle)) return;
+		
+		//TODO make that readable
+		float modifier = 1f - Mathf.Clamp01(Vector2.Dot(pushDirection2d, moveDirection2d) / (pushRadius + snowBall.radius));
+		snowBall.Push(modifier * ballPush * pushDirection3d.normalized);
 	}
 }
